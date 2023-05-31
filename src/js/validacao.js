b0633fbbc3aad1129bb6559a58bf72ac78fcc0b7
Oseias Nogueira <oseias.n.m@gmail.com>
2023-05-31 18:15:21 -0300

    Criada as validações dos campos: Nome, email e telefone

--- src/js/validacao.js
+++ src/js/validacao.js
@@ -1,53 +1,50 @@
-const enviarMensagem = document.getElementById("enviar");
+const formulario = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const mensagemCamposObrigatorios = document.querySelectorAll(".span-required")
+const validacaoEmail = /\S+@\S+\.\S+/;
+const validacaoTelefone = /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/;
 
-function validarCampoDigitado() {
-    var nome = document.getElementById("nome");
+function setError(index) {
+    campos[index].style.border = '2px solid #F52E2E';
+    mensagemCamposObrigatorios[index].style.display = 'block';
+}
 
-    if(nome.value != "") {
-        alert("Informou informado");
-        
+function setSucess(index) {
+    campos[index].style.border = '2px solid #00C22B';
+    mensagemCamposObrigatorios[index].style.display = 'none';
+}
+
+function validarNome() {
+    if(campos[0].value.length < 3){
+        setError(0);
+
+    }else {
+        setSucess(0);
+    }       
+}
+
+function validarEmail() {
+    if(validacaoEmail.test(campos[1].value)){
+        setSucess(1);
+
+    }else {
+        setError(1)
     }
 }
 
-enviarMensagem.addEventListener("click", validacao);
-
-function validacao(a) {
-    a.preventDefault();
-
-    const campoNome = document.getElementById("nome");
-    const campoEmail = document.getElementById("email");
-    const campoTelefone = document.getElementById("telefone");
-    const campoComentario = document.getElementById("comentario");
-    let valido = true;
-
-    if(!campoNome.value){
-        const erroNome = document.getElementById("erroNome");
-        erroNome.classList.add("visible");
-        campoNome.classList.add("invalid");
-        erroNome.setAttribute("aria-hidden", false);
-        erroNome.setAttribute("aria-invalid", true);
-
-        const erroEmail = document.getElementById("erroEmail");
-        erroEmail.classList.add("visible");
-        campoEmail.classList.add("invalid");
-        erroEmail.setAttribute("aria-hidden", false);
-        erroEmail.setAttribute("aria-invalid", true);
-
-        const erroTelefone = document.getElementById("erroTelefone");
-        erroTelefone.classList.add("visible");
-        campoTelefone.classList.add("invalid");
-        erroTelefone.setAttribute("aria-hidden", false);
-        erroTelefone.setAttribute("aria-invalid", true);
-
-        const erroComentario = document.getElementById("erroComentario");
-        erroComentario.classList.add("visible");
-        campoComentario.classList.add("invalid");
-        erroComentario.setAttribute("aria-hidden", false);
-        erroComentario.setAttribute("aria-invalid", true);
-        
+function validarTelefone() {
+    if(validacaoTelefone.test(campos[2].value)){
+        setSucess(2);
+    } else {
+        setError(2);
     }
-
-    return valido;
 }
 
-console.log(validarCampoDigitado)
+function validarMensagem() {
+    if(campos[3].value.length < 3) {
+        console.log("Não digitou nada");
+        setError(3)
+    } else {
+        setSucess(3);
+    }
+}
